cb0f8b3f60cb3c0386de2400265d803a5a780df9
benjamin <benjaminchen.c@gmail.com>
2016-07-06 12:25:32 +0800

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var webpack = require('webpack')
 
 module.exports = {
     entry: [
@@ -15,5 +16,18 @@ module.exports = {
             test: /\.js$/,
             loaders: ['babel']
         }]
-    }
+    },
+
+    plugins:[
+        new webpack.DefinePlugin({
+            'process.env':{
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress:{
+                warnings: false
+            }
+        })
+    ]
 };

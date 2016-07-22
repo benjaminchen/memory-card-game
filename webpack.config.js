0e8fccd5fac4b2d17d12629837819770aa1e1857
benjamin <benjaminchen.c@gmail.com>
2016-07-22 12:02:39 +0800

    update build project script

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     ],
 
     output: {
-        path: path.resolve(__dirname, 'example/assets/js'),
+        path: path.resolve(__dirname, 'release'),
         filename: 'memory.min.js'
     },
 
